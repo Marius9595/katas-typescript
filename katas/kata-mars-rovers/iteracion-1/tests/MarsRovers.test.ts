71aa6b0243205bd87d7d412fdd9ba196696f3b4b
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-10-07 17:16:36 +0100

    :test_tube:  Mars Rovers should do nothing without commands

--- katas/kata-mars-rovers/iteracion-1/tests/MarsRovers.test.ts
+++ katas/kata-mars-rovers/iteracion-1/tests/MarsRovers.test.ts
@@ -14,5 +14,17 @@ describe("Mars Rovers should",()=>{
 		expect(marsRovers.position).toStrictEqual(initialPosition);
 		expect(marsRovers.orientation).toStrictEqual(initialOrientation);
 	})
+
+	test("move forward", () =>{
+		const initialPosition = new Position(0,0);
+		const initialOrientation = new NorthFacing();
+		const marsRovers = MarsRovers.at(initialPosition, initialOrientation);
+
+		const moveForward = ['f'];
+		marsRovers.applyCommands(moveForward);
+
+		expect(marsRovers.position).toStrictEqual(new Position(0,1));
+		expect(marsRovers.orientation).toStrictEqual(initialOrientation);
+	})
 })
 
