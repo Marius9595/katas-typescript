711accea65aeb716a0e85cf871d916ea202bf752
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-10-14 16:02:36 +0100

    :white_check_mark: [property] Mars Rovers should move inside of grid

--- katas/kata-mars-rovers/iteracion-1/src/Grid.ts
+++ katas/kata-mars-rovers/iteracion-1/src/Grid.ts
@@ -1,16 +1,23 @@
 import Position from "./valueObjects/Position";
 import Orientation from "./orientation/Orientation";
 import NorthFacing from "./orientation/NorthFacing";
+import SouthFacing from "./orientation/SouthFacing";
+import WestFacing from "./orientation/WestFacing";
+import EastFacing from "./orientation/EastFacing";
 
 class Grid{
     private readonly _width: number;
     private readonly _height: number;
     private readonly _obstacles: Position[];
+    private xOrigin: number;
+    private yOrigin: number;
 
     constructor(obstacles: Position[] = []){
         this._width = 10;
         this._height = 10;
         this._obstacles = obstacles;
+        this.xOrigin = 0;
+        this.yOrigin = 0;
     }
 
     nextForwardPositionAccordingTo(position: Position, orientation: Orientation) {
@@ -19,22 +26,48 @@ class Grid{
         if(this.isThereAnObstacleIn(nextForwardPosition)){
             return position;
         }
-
-        if(orientation instanceof NorthFacing){
-            nextForwardPosition = nextForwardPosition.isEqualTo(new Position(0, this._height))?
-                new Position(0,0)
-                : nextForwardPosition;
-        }
+        nextForwardPosition = this.wrapWhenItIsMovingForwardToEdgeOfGrid(orientation, nextForwardPosition);
 
         return nextForwardPosition;
     }
 
+    private wrapWhenItIsMovingForwardToEdgeOfGrid(orientation: Orientation, nextForwardPosition: Position) {
+        if (orientation instanceof NorthFacing) {
+            nextForwardPosition = (
+                nextForwardPosition.isEqualTo(new Position(nextForwardPosition.x, this._width)) ?
+                    new Position(nextForwardPosition.x, this.yOrigin)
+                    : nextForwardPosition
+            );
+        } else if (orientation instanceof WestFacing) {
+            nextForwardPosition = (
+                nextForwardPosition.isEqualTo(new Position(this.xOrigin-1, nextForwardPosition.y)) ?
+                    new Position(this._width-1, nextForwardPosition.y)
+                    : nextForwardPosition
+            );
+        } else if(orientation instanceof SouthFacing){
+            nextForwardPosition = (
+                nextForwardPosition.isEqualTo(new Position(nextForwardPosition.x, this.yOrigin-1)) ?
+                    new Position(nextForwardPosition.x, this._height-1)
+                    : nextForwardPosition
+            );
+        }else if(orientation instanceof EastFacing){
+            nextForwardPosition = (
+                nextForwardPosition.isEqualTo(new Position(this._width, nextForwardPosition.y)) ?
+                    new Position(this.xOrigin, nextForwardPosition.y)
+                    : nextForwardPosition
+            );
+        }
+        return nextForwardPosition;
+    }
+
     nextBackwardPositionAccordingTo(position: Position, orientation: Orientation) {
-        const nextBackwardPosition=  orientation.nextBackwardPositionRespectTo(position);
+        let nextBackwardPosition=  orientation.nextBackwardPositionRespectTo(position);
 
         if(this.isThereAnObstacleIn(nextBackwardPosition)){
             return position;
         }
+
+        nextBackwardPosition = this.wrapWhenItIsMovingBackwardToEdgeOfGrid(orientation, nextBackwardPosition);
 
         return nextBackwardPosition;
     }
@@ -43,7 +76,34 @@ class Grid{
         return this._obstacles.some(obstacle => obstacle.isEqualTo(nextForwardPosition));
     }
 
-
+    private wrapWhenItIsMovingBackwardToEdgeOfGrid(orientation: Orientation, nextBackwardPosition: Position) {
+        if (orientation instanceof NorthFacing) {
+            nextBackwardPosition = (
+                nextBackwardPosition.isEqualTo(new Position(nextBackwardPosition.x, -1)) ?
+                    new Position(nextBackwardPosition.x, this._height-1)
+                    : nextBackwardPosition
+            );
+        } else if (orientation instanceof WestFacing) {
+            nextBackwardPosition = (
+                nextBackwardPosition.isEqualTo(new Position(this._width, nextBackwardPosition.y)) ?
+                    new Position(this.xOrigin, nextBackwardPosition.y)
+                    : nextBackwardPosition
+            );
+        } else if(orientation instanceof SouthFacing){
+            nextBackwardPosition = (
+                nextBackwardPosition.isEqualTo(new Position(nextBackwardPosition.x, this._height)) ?
+                    new Position(nextBackwardPosition.x, this.yOrigin)
+                    : nextBackwardPosition
+            );
+        } else if(orientation instanceof EastFacing){
+            nextBackwardPosition = (
+                nextBackwardPosition.isEqualTo(new Position(this.xOrigin-1, nextBackwardPosition.y)) ?
+                    new Position(this._width-1, nextBackwardPosition.y)
+                    : nextBackwardPosition
+            );
+        }
+        return nextBackwardPosition;
+    }
 }
 
 export default Grid
