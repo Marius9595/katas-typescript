d51e3d2402b96de278daeb3039004bd734df9d63
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-10-14 16:38:37 +0100

    :art: Improved structure of katas

--- katas/mars-rovers/iteracion-1/src/MarsRovers.ts
+++ katas/mars-rovers/iteracion-1/src/MarsRovers.ts
@@ -1,1 +1,66 @@
-export const marsRovers = (a, b) => a + b;
+import Position from "./valueObjects/Position";
+import Orientation from "./orientation/Orientation";
+import Grid from "./Grid";
+import NorthFacing from "./orientation/NorthFacing";
+
+class MarsRovers{
+    private constructor(
+        private _position: Position,
+        private _orientation: Orientation,
+        private readonly _grid: Grid
+    ) {
+    }
+
+    applyCommands(commands: string[]) {
+        if(commands.length === 0){
+            return `${this._position.x}:${this._position.y}:${this._orientation.cardinalSymbol()}`
+        }
+
+        const firstCommand = commands[0];
+        const restCommands = commands.slice(1);
+        if(this.hasToMoveForwardDueTo(firstCommand)){
+            const newPosition = this._grid.nextForwardPositionAccordingTo(this._position, this._orientation);
+            if(newPosition.isEqualTo(this._position)){
+                return `O:${this._position.x}:${this._position.y}:${this._orientation.cardinalSymbol()}`
+            }
+            this._position = newPosition;
+        }else if(this.hasToMoveBackwardDueTo(firstCommand)){
+            const newPosition = this._grid.nextBackwardPositionAccordingTo(this._position, this._orientation);
+            if(newPosition.isEqualTo(this._position)){
+                return `O:${this._position.x}:${this._position.y}:${this._orientation.cardinalSymbol()}`
+            }
+            this._position = newPosition;
+        }else if(this.hasToTurnleftDueTo(firstCommand)){
+            this._orientation = this._orientation.toTheLeft();
+        }else if(this.hasToTurnRightDueTo(firstCommand)){
+            this._orientation = this._orientation.toTheRight();
+        }
+        return this.applyCommands(restCommands);
+    }
+
+    private hasToTurnRightDueTo(firstCommand: string) {
+        return firstCommand == 'r';
+    }
+
+    private hasToTurnleftDueTo(firstCommand: string) {
+        return firstCommand == 'l';
+    }
+
+    private hasToMoveBackwardDueTo(firstCommand: string) {
+        return firstCommand == 'b';
+    }
+
+    private hasToMoveForwardDueTo(firstCommand: string) {
+        return firstCommand === 'f';
+    }
+
+    static in(grid: Grid) {
+        return new MarsRovers(
+            new Position(0, 0),
+            new NorthFacing(),
+            grid
+        );
+    }
+}
+
+export default MarsRovers
--- katas/mars-rovers/iteracion-1/tests/MarsRovers.test.ts
+++ katas/mars-rovers/iteracion-1/tests/MarsRovers.test.ts
@@ -1,8 +1,183 @@
-import { marsRovers } from '../src/./MarsRovers';
+import fc from 'fast-check';
 
-test('should marsRovers two numbers', () => {
-	const result = marsRovers(1, 2);
-	const expected = 3;
+import Position from "../src/valueObjects/Position";
+import MarsRovers from "../src/MarsRovers";
+import Grid from "../src/Grid";
 
-	expect(result).toBe(expected);
-});
+describe("Mars Rovers should",()=>{
+	let itIsInTheInitialPositionAndOrientation: string
+	let marsRovers: MarsRovers;
+	beforeEach(() => {
+		itIsInTheInitialPositionAndOrientation = "0:0:N";
+		marsRovers = MarsRovers.in(new Grid());
+	})
+
+	test("do nothing without commands", ()=>{
+		const noCommands = [];
+
+		const response = marsRovers.applyCommands(noCommands);
+
+		expect(response).toBe(itIsInTheInitialPositionAndOrientation);
+	})
+
+	describe("execute the single commands:",()=>{
+		test("move forward", () =>{
+			const moveForward = ['f'];
+
+			const response = marsRovers.applyCommands(moveForward);
+
+			expect(response).toBe('0:1:N');
+		})
+
+		test("move backward", () => {
+			const moveBackward = ['b'];
+
+			const response = marsRovers.applyCommands(moveBackward);
+
+			expect(response).toBe('0:9:N');
+		})
+
+		test("turn left", () => {
+			const turnLeft = ['l'];
+
+			const response = marsRovers.applyCommands(turnLeft);
+
+			expect(response).toBe('0:0:W');
+		})
+
+		test("turn right", () => {
+			const turnRight = ['r'];
+
+			const response = marsRovers.applyCommands(turnRight);
+
+			expect(response).toBe('0:0:E');
+		})
+	})
+
+	test('move inside of grid',()=>{
+		const commands = (
+			fc.array(
+				fc.constantFrom('f', 'b', 'l', 'r'),
+				{minLength: 1}
+			)
+		);
+
+		fc.assert(
+			fc.property(commands, (commands) => {
+				const marsRovers = MarsRovers.in(new Grid());
+
+				const response = marsRovers.applyCommands(commands);
+
+				expect(response).toMatch(/^[0-9]:[0-9]:[N|S|E|W]$/);
+			})
+		)
+	})
+
+	test('be inside of grid when is moving backward or forward north facing',()=>{
+		const commands = (
+			fc.array(
+				fc.constantFrom('f', 'b'),
+				{minLength: 1}
+			)
+		);
+
+		fc.assert(
+			fc.property(commands, (commands) => {
+				const marsRovers = MarsRovers.in(new Grid());
+
+				const response = marsRovers.applyCommands(commands);
+
+				expect(response).toMatch(/^0:[0-9]:N$/);
+			})
+		)
+	})
+
+	test('be inside of grid when is moving backward or forward south facing',()=>{
+		const commands = (
+			fc.array(
+				fc.constantFrom('f', 'b'),
+				{minLength: 1}
+			)
+		);
+
+		fc.assert(
+			fc.property(commands, (commands) => {
+				const marsRovers = MarsRovers.in(new Grid());
+				const faceSouth = ['r','r'];
+
+				const response = marsRovers.applyCommands(
+					faceSouth.concat(commands)
+				);
+
+				expect(response).toMatch(/^0:[0-9]:S$/);
+			})
+		)
+	})
+
+	test('be inside of grid when is moving backward or forward west facing',()=>{
+		const commands = (
+			fc.array(
+				fc.constantFrom('f', 'b'),
+				{minLength: 1}
+			)
+		);
+
+		fc.assert(
+			fc.property(commands, (commands) => {
+				const marsRovers = MarsRovers.in(new Grid());
+				const faceWest = ['l'];
+
+				const response = marsRovers.applyCommands(
+					faceWest.concat(commands)
+				);
+
+				expect(response).toMatch(/^[0-9]:0:W$/);
+			})
+		)
+	})
+
+	test('be inside of grid when is moving backward or forward east facing',()=>{
+		const commands = (
+			fc.array(
+				fc.constantFrom('f', 'b'),
+				{minLength: 1}
+			)
+		);
+
+		fc.assert(
+			fc.property(commands, (commands) => {
+				const marsRovers = MarsRovers.in(new Grid());
+				const faceEast = ['r'];
+
+				const response = marsRovers.applyCommands(
+					faceEast.concat(commands)
+				);
+
+				expect(response).toMatch(/^[0-9]:0:E$/);
+			})
+		)
+	})
+
+	//TODO: Put a obstacle in the initial position of mars rovers should not be allowed
+
+	test('detect obstacles when is moving to a forward position and report its last position', ()=>{
+		const obstacles = [new Position(0,2)];
+		const marsRovers = MarsRovers.in(new Grid(obstacles));
+		const moveForwardMultipleTimes = ['f','f','f'];
+
+		const response = marsRovers.applyCommands(moveForwardMultipleTimes);
+
+		expect(response).toBe('O:0:1:N');
+	})
+
+	test('detect obstacles when is moving to a backward position and report its last position', ()=>{
+		const obstacles = [new Position(0,8)];
+		const marsRovers = MarsRovers.in(new Grid(obstacles));
+		const moveForwardMultipleTimes = ['b','b','b'];
+
+		const response = marsRovers.applyCommands(moveForwardMultipleTimes);
+
+		expect(response).toBe('O:0:9:N');
+	})
+})
+
